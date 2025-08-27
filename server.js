09969f9690d3c8d60ca46f48fad3c645d1bf4d6a
Никита Басалаев <n.basalaev@GKVERTIKAL.LOCAL>
2025-08-27 12:49:07 +0300

    remade productsChars

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ const getMoscowTime = () => {
 //limiter запросов
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 45 minutes
+    windowMs: 15 * 60 * 1000, // 15 minutes
     limit: 1, // Limit each IP to 100 requests per `window` (here, per 1 minute).
     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
